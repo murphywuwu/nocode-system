49e176beaf36dc02adbd687e686dc8fa85c50df1
murphywuwu <murphywuwu@gmail.com>
2022-04-19 15:51:50 +0800

    :construction: (packages/server/src/app.ts) use koa-compose with middleware

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,25 +1,10 @@
 import Koa from 'koa';
-import router from './router';
-const bodyParser = require('koa-bodyparser');
+import compose from 'koa-compose';
+
+import middlewares from './middlewares';
 
 const app = new Koa();
 
-app.use(bodyParser());
-app.use(router.routes());
-/*
-  原先当路由存在，请求方式不匹配的时候，会报404，
-  加了这个中间件，会报请求方式不被允许
-*/
-app.use(router.allowedMethods());
-
-app.use(async function (ctx, next) {
-  await next();
-
-  ctx.body = {
-    statusCode: ctx.status,
-    code: (ctx.body && ctx.body.code) || null,
-    msg: (ctx.body && ctx.body.msg) || ctx.response.message,
-  };
-});
+app.use(compose(middlewares));
 
 export default app;
